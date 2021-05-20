7aeed7eff4f19a71a807f92b3349c44341823e04
Wendy Beck <webeck@gmail.com>
2021-05-19 19:22:38 -0700

    passing movie list correctly

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,8 +16,8 @@ const Main = props => {
 
 
     const handleChange = async e => {
-        const query = encodeURI(e.target.value)
-        console.log('query:', query);
+        let query = encodeURI(e.target.value)
+        console.log('e.target.value:', query);
         return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`)
             .then(response => {
                 if (response.status === 200) {
@@ -27,15 +27,19 @@ const Main = props => {
                 }
             })
             .then(resp => {
+                console.log('resp:', resp);
                 setMovieList([
                     ...movieList,
                     resp.results
                 ])
-                const filtered = movieList.filter(movie =>{
-                    console.log('movie:', movie);
-											
-                })
-                console.log('filtered:', filtered);
+                console.log('movieList:', movieList.length);
+                query = decodeURI(query)
+                // const filtered = movieList.results.filter(movie =>{
+                //     console.log('movie:', movie);
+                //     const filter = (movie.indexOf(query)>-1)	
+                //     console.log('movie.indexOf(query)>-1:', movie.title,movie.indexOf(query)>-1);
+                // })
+                // console.log('filtered:', filtered);
             })
             .catch(error => {
                 console.error(error);
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import {Card} from '@material-ui/core'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const MovieList = props => {
-    console.log('props in MovieList:', props);
-    return <div>Movie list goes heres</div>
+    const {options} = props
+    console.log('movie List in ML:', options ? options : 0);
+	
+    return (
+        <div>
+            <InfiniteScroll
+                dataLength={options ? options.length : 0}
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>
+                }
+            >
+                {options[0] !== undefined &&
+									options[0].forEach(i=>{
+			    				console.log('i:', i);	
+			    				return <Card key={(i.id+2) * Math.random()}>{i.title}</Card>
+									})
+                }
+            </InfiniteScroll>
+        </div>	
+    )		
 }
-
+	
 export default MovieList
